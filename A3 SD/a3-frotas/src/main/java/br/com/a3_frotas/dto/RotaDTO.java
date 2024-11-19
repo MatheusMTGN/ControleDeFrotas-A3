f77882bf7fb7d8f3f117705e4b1897997c929445
Anna <133595164+weepkyn@users.noreply.github.com>
2024-11-19 18:42:46 -0300

    Delete A3 SD/a3-frotas/src/main/java/br/com/a3_frotas/dto directory

--- A3 SD/a3-frotas/src/main/java/br/com/a3_frotas/dto/RotaDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.a3_frotas.dto;
-
-public record RotaDTO(
-        String partida,
-        String destino
-        //DescricaoDTO descricao
-) {
-}
--- A3 SD/a3-frotas/src/main/java/br/com/a3_frotas/dto/VeiculoDTO.java
+++
@@ -1,8 +0,0 @@
-package br.com.a3_frotas.dto;
-
-public record VeiculoDTO(
-        int ano,
-        String modelo,
-        String placa
-) {
-}
