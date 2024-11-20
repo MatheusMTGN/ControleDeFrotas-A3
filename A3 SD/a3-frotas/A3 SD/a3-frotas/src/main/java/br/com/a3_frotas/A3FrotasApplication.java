caa7ee5fb435fc009cdaca2675f086b8020beb49
Anna <133595164+weepkyn@users.noreply.github.com>
2024-11-20 16:52:21 -0300

    Add files via upload
    OBS: Endpoints não testados.

---
+++ A3 SD/a3-frotas/A3 SD/a3-frotas/src/main/java/br/com/a3_frotas/A3FrotasApplication.java
@@ -0,0 +1,13 @@
+package br.com.a3_frotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A3FrotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A3FrotasApplication.class, args);
+	}
+
+}
---
+++ A3 SD/a3-frotas/A3 SD/a3-frotas/src/test/java/br/com/a3_frotas/A3FrotasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.a3_frotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A3FrotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
